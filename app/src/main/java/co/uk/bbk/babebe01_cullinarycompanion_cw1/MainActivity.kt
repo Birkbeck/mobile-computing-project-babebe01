77e6756969ce3e08474617e59d8b317d7cbc5c5b
Bestman Abebe <bestmanabebe@yahoo.co.uk>
2025-06-01 08:05:52 +0100

    Refactor: Update dependencies and UI elements
    This commit includes the following changes:
    
    - Updated Android Gradle Plugin to version 8.2.0.
    - Updated Gradle wrapper to version 8.2.
    - Cleaned up `settings.gradle.kts` by removing redundant repository configurations.
    - Added `RecipeDetailsActivity` to `AndroidManifest.xml`.
    - Updated `MainActivity.kt` to launch `RecipeDetailsActivity` on FAB click.
    - Expanded color definitions in `colors.xml`.
    - Updated application theme in `themes.xml` to use MaterialComponents.
    - Adjusted constraints in `activity_main.xml`.
    - Updated deployment target selector configurations.

--- app/src/main/java/co/uk/bbk/babebe01_cullinarycompanion_cw1/MainActivity.kt
+++ app/src/main/java/co/uk/bbk/babebe01_cullinarycompanion_cw1/MainActivity.kt
@@ -1,7 +1,8 @@
 package co.uk.bbk.babebe01_cullinarycompanion_cw1
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -9,12 +10,18 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { view, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener {
+            val intent = Intent(this, RecipeDetailsActivity::class.java)
+            startActivity(intent)
         }
     }
 }
